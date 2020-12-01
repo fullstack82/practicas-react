6817ae9f800499e0749e717aa0381c1db8d4a4df
fullstack82 <fullstack8210@gmail.com>
2020-12-01 14:49:54 +0100

    react-2

---
+++ src/UserViewer.js
@@ -0,0 +1,21 @@
+import UseFetch from './UseFetch'
+
+function Menu() {
+
+    const users = UseFetch('https://jsonplaceholder.typicode.com/users')
+
+    if (!users) return 'Loading ...'
+
+    return (
+        <aside className="menu">
+            <h2>Menu</h2>
+            <ul>
+                {users.map(user =>
+                    <li key={user.id}>{user.name}</li>
+                )}
+            </ul>
+        </aside>
+    )
+}
+
+export default Menu
